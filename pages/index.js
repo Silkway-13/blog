3517568426148b41b061c99531fbce20ce057e36
bayr.khan <bayrsaikhan.ger13@gmail.com>
2023-08-15 16:05:14 +0800

    react boosttrap connect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Alert from "react-bootstrap/Alert";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>my blog</div>;
+  return (
+    <div>
+      {[
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </div>
+  );
 }
